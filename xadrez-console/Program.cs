006ea50a1c70dec684770297a9a0011e94ef1fa9
Natal Lima <nataldelima@gmail.com>
2024-12-09 19:24:51 -0400

    Começando a implementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,9 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Posicao P;
-        P = new Posicao(3, 4);
-        Console.WriteLine("Posição: "+ P);
+        Tabuleiro tab = new Tabuleiro(8, 8);
         Console.ReadLine();
     }
 }
---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,12 @@
+namespace tabuleiro;
+
+public enum Cor
+{
+    Branca,
+    Preta,
+    Amarela,
+    Azul,
+    Vermelha,
+    Verde,
+    Laranja
+}
